08b524ae4cd56322f70c4e1c34d529c45a6181f8
unknown <nguyenquochuydl123@gmail.com>
2024-09-14 23:41:27 +0700

    [mobile] add check-otp-acti

--- Hd.Wallet.Android/app/src/main/java/com/socialv2/ewallet/ui/register/RegisterCheckOtpActivity.java
+++ Hd.Wallet.Android/app/src/main/java/com/socialv2/ewallet/ui/register/RegisterCheckOtpActivity.java
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.socialv2.ewallet.R;
 
-public class EnterOtpActivity extends AppCompatActivity {
+public class RegisterCheckOtpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_enter_otp);
+        setContentView(R.layout.activity_register_check_otp);
 
         initView();
     }
