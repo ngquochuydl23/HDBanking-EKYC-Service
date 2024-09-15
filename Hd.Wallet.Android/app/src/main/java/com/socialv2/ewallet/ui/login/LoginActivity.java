41e7fd977e46ce8a50b8d56a54927e0d8c65071f
stanleylover4444 <StanleyInTech@gmail.com>
2024-09-15 09:38:33 +0700

    design ui login

---
+++ Hd.Wallet.Android/app/src/main/java/com/socialv2/ewallet/ui/login/LoginActivity.java
@@ -0,0 +1,52 @@
+package com.socialv2.ewallet.ui.login;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.socialv2.ewallet.R;
+import com.socialv2.ewallet.ui.register.DetailedAddressAtivity;
+import com.socialv2.ewallet.ui.register.RegisterActivity;
+import com.socialv2.ewallet.ui.register.SignUpAccountActivity;
+import com.socialv2.ewallet.utils.NavigateUtil;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button mContinue  , mSignIn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        mContinue = findViewById(R.id.continueButton);
+        mSignIn = findViewById(R.id.signinButton);
+
+
+        initView();
+    }
+
+    private void initView() {
+        mContinue.setOnClickListener(view -> {
+            NavigateUtil.navigateTo(this, LoginPasswordActivity.class);
+        });
+
+        mSignIn.setOnClickListener(view -> {
+            NavigateUtil.navigateTo(this, RegisterActivity.class);
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
