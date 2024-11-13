0943283d12fedbd952ba2c690f77473c45f1b349
unknown <nguyenquochuydl123@gmail.com>
2024-11-13 18:29:11 +0700

    baseDialog

---
+++ Hd.Wallet.Android/app/src/main/java/com/socialv2/ewallet/BaseDialog.java
@@ -0,0 +1,70 @@
+package com.socialv2.ewallet;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.LayoutRes;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+
+public abstract class BaseDialog {
+
+    private Context mContext;
+    private MaterialDialog mMaterialDialog;
+    private DialogStyle mDialogStyle;
+
+
+    public BaseDialog(Context context, @LayoutRes int layout) {
+        mContext = context;
+        mMaterialDialog = new MaterialDialog(mContext, MaterialDialog.getDEFAULT_BEHAVIOR());
+
+        onDialogCreated(LayoutInflater
+                .from(context)
+                .inflate(layout, null));
+    }
+
+    public void show() {
+        if (!mMaterialDialog.isShowing()) {
+            mMaterialDialog.show();
+        }
+    }
+
+    public void dismiss() {
+        if (mMaterialDialog.isShowing()) {
+            mMaterialDialog.dismiss();
+        }
+    }
+
+    protected abstract void onDialogCreated(View view);
+
+    public BaseDialog setupStyle(DialogStyle style) {
+        mDialogStyle = style != null ? style : new DialogStyle();
+
+        mMaterialDialog.cornerRadius(mDialogStyle.getBorderRadius(), null);
+        return this;
+    }
+
+    public class DialogStyle {
+
+        private float borderRadius;
+
+
+        public DialogStyle() {
+            this.borderRadius = 5f;
+        }
+
+        public DialogStyle(float borderRadius) {
+            this.borderRadius = borderRadius;
+        }
+
+        public void setBorderRadius(float borderRadius) {
+            this.borderRadius = borderRadius;
+        }
+
+        public float getBorderRadius() {
+            return borderRadius;
+        }
+    }
+}
---
+++ Hd.Wallet.Android/app/src/main/java/com/socialv2/ewallet/components/OkDialog.java
@@ -0,0 +1,19 @@
+package com.socialv2.ewallet.components;
+
+import android.content.Context;
+import android.view.View;
+
+import com.socialv2.ewallet.BaseDialog;
+import com.socialv2.ewallet.R;
+
+public class OkDialog extends BaseDialog {
+
+    public OkDialog(Context context) {
+        super(context, R.layout.layout_code_picker);
+    }
+
+    @Override
+    protected void onDialogCreated(View view) {
+
+    }
+}
