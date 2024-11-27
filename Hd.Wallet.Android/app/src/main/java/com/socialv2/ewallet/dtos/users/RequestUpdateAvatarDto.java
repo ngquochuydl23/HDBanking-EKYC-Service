acc3acc06e8fb133f21777a9ffd9acd54fffd060
unknown <nguyenquochuydl123@gmail.com>
2024-11-27 08:15:10 +0700

    profile

--- Hd.Wallet.Android/app/src/main/java/com/socialv2/ewallet/dtos/users/RequestUpdateAvatarDto.java
+++ Hd.Wallet.Android/app/src/main/java/com/socialv2/ewallet/dtos/users/RequestUpdateAvatarDto.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 
 public class RequestUpdateAvatarDto {
 
-    @SerializedName("id")
+    @SerializedName("avatar")
     @Expose
     public String avatar;
 
